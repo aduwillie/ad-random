bee7122faa8bc1ac674d09e909318b8a00b42197
Adu Williams <aduwillie@gmail.com>
2017-12-06 14:23:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const uuid = require('node-uuid');
+const crypto = require('crypto');
+
+const random = (min, max, inclusive) => {
+    const difference = inclusive ? (max - min + 1) : (max - min);
+    return Math.random() * difference + min;
+}
+
+const randomHex = (length) => {
+    return crypto.randomBytes(Math.ceil(length / 2))
+        .toString('hex')
+        .slice(0, length);
+}
+
+const randomBase64 = (length) => {
+    return crypto.randomBytes(Math.ceil(length *  3 / 4))
+        .toString('base64')
+        .slice(0, length)
+        .replace(/\+/g, '0')
+        .replace(/\//g, '0');
+}
+
+module.exports = {
+    random,
+    uuid,
+    randomHex,
+    randomBase64,
+}
